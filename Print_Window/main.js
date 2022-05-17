47435e4be14fde4daee9f2ecfe048f8fb5cf8e07
NICO <98567348+nicoprog4@users.noreply.github.com>
2022-05-17 16:32:32 -0500

    Add files via upload

---
+++ Print_Window/main.js
@@ -0,0 +1,12 @@
+
+/*Declare the variable for the function print and the function declare the variable for to call the Id print, then create 
+the event for the button and finally to create the function print.
+
+
+*/
+function printCurrentPage()
+{
+window.print();
+}
+// To call the function to print the window active
+//printCurrentWindow();
